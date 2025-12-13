51d34a8f641efe46a30fe71dbbb4cee3bc2e2cb9
Pmtal3122 <talegaonkar.piyush@gmail.com>
2025-12-13 14:31:26 +0530

    Passing json as a message

--- src/main/java/com/kfkex/KafkaExample/controllers/TryController.java
+++ src/main/java/com/kfkex/KafkaExample/controllers/TryController.java
@@ -3,12 +3,18 @@ package com.kfkex.KafkaExample.controllers;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import tools.jackson.databind.JsonNode;
+import tools.jackson.databind.json.JsonMapper;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.concurrent.CompletableFuture;
 
 @RestController
@@ -17,12 +23,25 @@ import java.util.concurrent.CompletableFuture;
 @Slf4j
 public class TryController {
 
+    @Value("classpath:data/tryJson.json")
+    private Resource tryJson;
+
     private final KafkaTemplate<@NonNull Integer, @NonNull String> kafkaTemplate;
+    private final JsonMapper jsonMapper;
 
     @PostMapping
-    public void sendMessage() {
+    public void sendMessage() throws IOException {
+
+//        log.info("tryJson: {}", tryJson);
+//        log.info("tryJson bytes {}", Files.readAllBytes(tryJson.getFilePath()));
+//        log.info("tryJson jsonNode {}", jsonMapper.readValue(Files.readAllBytes(tryJson.getFilePath()), JsonNode.class).toPrettyString());
+//        log.info("tryJson string {}", jsonMapper.writeValueAsString(Files.readAllBytes(tryJson.getFilePath())));
+
+        JsonNode jsonNode = jsonMapper.readValue(Files.readAllBytes(tryJson.getFilePath()), JsonNode.class);
+        String message = jsonNode.toString();
+
         log.info("[sendMessage] Sending message to kafka");
-        CompletableFuture<SendResult<@NonNull Integer, @NonNull String>> sendingTemplateMessage = kafkaTemplate.send("topic1", "Sending template message");
+        CompletableFuture<SendResult<@NonNull Integer, @NonNull String>> sendingTemplateMessage = kafkaTemplate.send("topic1", message);
         sendingTemplateMessage.whenCompleteAsync((result, ex) -> {
             log.info("The result is {}", result.getProducerRecord().value());
         });
--- src/main/java/com/kfkex/KafkaExample/listener/TryListener.java
+++ src/main/java/com/kfkex/KafkaExample/listener/TryListener.java
@@ -1,18 +1,38 @@
 package com.kfkex.KafkaExample.listener;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
+import tools.jackson.databind.JsonNode;
+import tools.jackson.databind.json.JsonMapper;
+
+import java.util.List;
 
 @Service
 @Slf4j
+@RequiredArgsConstructor
 public class TryListener {
 
     private static final String GROUP_1 = "group1";
+    private final JsonMapper jsonMapper;
 
     @KafkaListener(topics = "topic1", groupId = "group1", id = "listener1", autoStartup = "true", containerFactory = "kafkaListenerContainerFactory")
     public void listen(String data) {
         log.info("[tryListener] Group: {}, Message: {}", GROUP_1, data);
+        JsonNode jsonNode = jsonMapper.convertValue(data, JsonNode.class); // Wrong one
+
+        JsonNode jsonNode1 = jsonMapper.readTree(data); // Correct one
+
+//        log.info("[tryListener] jsonNode1: {}", jsonNode1.toPrettyString());
+
+        List<String> valuesList = jsonNode1.get("data")
+                .get("messages")
+                .valueStream()
+                .map(node -> node.get("value").toString())
+                .toList();
+
+        log.info("[tryListener] Values List: {}", valuesList);
     }
 }
 
